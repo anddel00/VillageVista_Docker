5e69488373e7a16d2610f6f02a8fda25fb4efad4
Andrea Del Fatto <andrea.delfatto@edu.unife.it>
2026-04-27 10:36:55 +0200

    migrazione da mysql a postgresql

--- src/main/java/com/villagevista/villagevista/services/config/Configuration.java
+++ src/main/java/com/villagevista/villagevista/services/config/Configuration.java
@@ -7,19 +7,21 @@ import com.villagevista.villagevista.Model.Dao.DAOFactory;
 
 public class Configuration {
     /* Database Configruation */
-    public static final String DAO_IMPL= DAOFactory.MYSQLJDBCIMPL;
-    public static final String DATABASE_DRIVER="com.mysql.cj.jdbc.Driver";
-    public static final String SERVER_TIMEZONE= Calendar.getInstance().getTimeZone().getID();
+    // L'Agente punta ora alla nuova famiglia di DAO per Postgres
+    public static final String DAO_IMPL = DAOFactory.POSTGRESJDBCIMPL;
 
-    public static final String DATABASE_URL="jdbc:mysql://villagevista-andreadelfatto-a5e9.f.aivencloud.com:21300/villaggio?user=avnadmin&password=AVNS_cBodN9VpSMTr_p4_VCO&sslMode=REQUIRED&serverTimezone=" + SERVER_TIMEZONE;
+    // Aggiornato al driver PostgreSQL
+    public static final String DATABASE_DRIVER = "org.postgresql.Driver";
+    public static final String SERVER_TIMEZONE = Calendar.getInstance().getTimeZone().getID();
+
+    // Nuova stringa di connessione JDBC per l'istanza PostgreSQL su Aiven
+    public static final String DATABASE_URL = "jdbc:postgresql://pg-villagevista-andreadelfatto-a5e9.i.aivencloud.com:21300/defaultdb?user=avnadmin&password=AVNS_apPNXaLImyhVFY0bLcU&sslmode=require";
+
     /* Session Configuration */
-    public static final String COOKIE_IMPL=DAOFactory.COOKIEIMPL;
+    public static final String COOKIE_IMPL = DAOFactory.COOKIEIMPL;
 
     /* Logger Configuration */
-    public static final String GLOBAL_LOGGER_NAME="VillageVista";
-    public static final String GLOBAL_LOGGER_FILE="villagevista_log.%g.%u.txt";
-    public static final Level GLOBAL_LOGGER_LEVEL=Level.ALL;
-
+    public static final String GLOBAL_LOGGER_NAME = "VillageVista";
+    public static final String GLOBAL_LOGGER_FILE = "villagevista_log.%g.%u.txt";
+    public static final Level GLOBAL_LOGGER_LEVEL = Level.ALL;
 }
-
-
